384d64f8c7132b3324157d85e8797417b28c91c8
Rob Guttman <rob.guttman@macmillan.com>
2022-03-14 09:51:31 -0400

    Cleanup some comments and error messages

--- puzzle.py
+++ puzzle.py
@@ -20,37 +20,38 @@ class Puzzle:
 
     def __init__(self, path):
         """
-        Load a puzzle from a text file in the format produced by https://github.com/joshbduncan/word-search-generator
+        Load a puzzle from a text file in the format produced by
+        https://github.com/joshbduncan/word-search-generator
         """
         self.rows = []
         with open(path, "r") as f:
             lines = f.readlines()
 
-            # find grid
-            for i, line in enumerate(lines[2:], 2):
-                row = line.strip("\n").split(",")
-                if len(row) <= 1:
-                    break
-                self.rows.append(row)
+        # find grid
+        for line in lines[2:]:
+            row = line.strip("\n").split(",")
+            if len(row) <= 1:
+                break
+            self.rows.append(row)
 
-            # find words
-            for j, line in enumerate(lines[i:], 2):
-                if "Find these words:" in line:
-                    part = line.split(":", 1)[1].strip(' "\n')
-                    self.words = part.split(", ")
-                    break
+        # find words
+        for line in lines:
+            if "Find these words:" in line:
+                part = line.split(":", 1)[1].strip(' "\n')
+                self.words = part.split(", ")
+                break
 
-            # find answers (used to validate the solution)
-            for line in lines[i + j :]:
-                if "Answer Key:" in line:
-                    self.answers = dict(re.findall(r"([A-Z]+) ([NSEW]{1,2} @ \([0-9]+, [0-9]+\))", line))
-                    break
+        # find answers (used to validate the solution)
+        for line in lines:
+            if "Answer Key:" in line:
+                self.answers = dict(re.findall(r"([A-Z]+) ([NSEW]{1,2} @ \([0-9]+, [0-9]+\))", line))
+                break
 
         # validate
         if not self.rows:
-            raise Exception("did not find rows")
+            raise Exception("rows not found")
         if not self.words:
-            raise Exception("did not find words")
+            raise Exception("words not found")
 
     def __str__(self):
         return f"Puzzle: {self.size} size, {len(self.words)} words"
@@ -63,10 +64,11 @@ class Puzzle:
         """
         Answers must be dicts with the word as the key and the answer as the value.
         Answer syntax is as found in https://github.com/joshbduncan/word-search-generator
-        Example: "E @ (12, 6)" means start at y=12, x=6 (1-indexed) and read Eastward
+        Example: "E @ (12, 6)" means start at 12 down and 6 across, then read Eastward
         """
         if answers:
             header = "** WORD SEARCH PUZZLE: ANSWERS **"
+            words = ", ".join(f"{word} {location}" for word, location in answers.items())
 
             # create answer grid
             rows = [["." for _ in range(self.size)] for _ in range(self.size)]  # blank grid
@@ -78,11 +80,11 @@ class Puzzle:
                         rows[y][x] = letter
                         x += self.DIRECTIONS[direction]["x"]
                         y += self.DIRECTIONS[direction]["y"]
-            words = ", ".join(f"{word} {location}" for word, location in answers.items())
         else:
             header = "** WORD SEARCH PUZZLE **"
-            rows = self.rows
             words = ", ".join(self.words)
+            rows = self.rows
+
         grid = "\n".join(" ".join(cell for cell in row) for row in rows)
         print(f"{header}\n\n{grid}\n\nWords: {words}\n")
 
@@ -97,7 +99,7 @@ class Puzzle:
         return answers
 
     def find_word(self, word):
-        """Return the x, y coordinates and direction of the word's location in the puzzle grid."""
+        """Return a string of the x, y coordinates and direction of the word's location in the puzzle grid."""
         for y in range(self.size):
             for x in range(self.size):
                 cell = self.get_cell(x, y)
@@ -115,7 +117,7 @@ class Puzzle:
         y += self.DIRECTIONS[direction]["y"]
         cell = self.get_cell(x, y)
         if not cell or cell != rest[0]:
-            return None
+            return False
         return self.find_rest(direction, x, y, rest[1:])
 
 
